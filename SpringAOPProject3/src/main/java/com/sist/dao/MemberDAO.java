c6941e76bf7e13a7ab6f129adc0a41674a55aec6
SIST <SIST@DESKTOP-I8FQNHB>
2020-11-18 16:24:10 +0900

    1

---
+++ SpringAOPProject3/src/main/java/com/sist/dao/MemberDAO.java
@@ -0,0 +1,33 @@
+package com.sist.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberDAO {
+	@Autowired
+	public MemberMapper mapper;
+	public String isLogin(String id, String pwd)
+	{
+		
+		String result="";
+		int count=mapper.idCheck(id);
+		if(count==0)
+		{
+			result="NOID";
+		}
+		else
+		{
+			String db_pwd=mapper.memberGetPasword(id);
+			if(db_pwd.equals(pwd))
+			{
+						result="OK";
+			}
+			else
+			{
+				result="NOPWD";
+			}
+		}
+		return result;
+	}
+}
